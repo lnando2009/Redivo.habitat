385a14dde3e083494da2fff3369b877e8cfb198a
lnando2009 <41671@raphaeldisanto.com.br>
2026-04-27 10:58:53 -0300

    Add files via upload

---
+++ model/Animal.java
@@ -0,0 +1,14 @@
+package model;
+
+import enums.Habitat;
+
+public class Animal {
+    
+    private String name;
+    private int speed;
+    private int age;
+    private double whight;
+    private Habitat habitat;
+    private boolean isWild;
+
+}
